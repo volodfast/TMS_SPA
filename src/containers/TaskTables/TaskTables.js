62020d9ee2323ef7d81616897ac9c4f4e33dc3b2
Volodymyr Bystranivskyi <vova.b@milestep.io>
2018-07-20 16:15:07 +0300

    Switch logic and represantation in TaskTables and TaskTablesContainer

--- src/containers/TaskTables/TaskTables.js
+++ src/containers/TaskTables/TaskTables.js
@@ -1,91 +1,54 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 
 import ActiveTaskTable from "./ActiveTaskTable/ActiveTaskTable";
 import FinishedTaskTable from "./FinishedTaskTable/FinishedTaskTable";
 
-import * as actions from "../../store/actions/actions";
+const TaskTables = props => {
+  const activeId = "active_tasks_container";
+  const finishedId = "finished_tasks_container";
 
-class TaskTables extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClickActive = this.handleClickActive.bind(this);
-    this.handleClickFinished = this.handleClickFinished.bind(this);
+  let activeTabStyle = "";
+  let activeIsActive = false;
+  if (props.activeTab === "active") {
+    activeTabStyle = "active";
+    activeIsActive = true;
   }
 
-  handleClickActive(e) {
-    this.props.changeTab("active");
+  let finishedTabStyle = "";
+  let finishedIsActive = false;
+  if (props.activeTab === "finished") {
+    finishedTabStyle = "active";
+    finishedIsActive = true;
   }
 
-  handleClickFinished(e) {
-    this.props.changeTab("finished");
-  }
-
-  render() {
-    const activeId = "active_tasks_container";
-    const finishedId = "finished_tasks_container";
-
-    let activeTabStyle = "";
-    let activeIsActive = false;
-    if (this.props.activeTab === "active") {
-      activeTabStyle = "active";
-      activeIsActive = true;
-    }
-
-    let finishedTabStyle = "";
-    let finishedIsActive = false;
-    if (this.props.activeTab === "finished") {
-      finishedTabStyle = "active";
-      finishedIsActive = true;
-    }
-
-    return (
-      <div className="col-md-8">
-        <ul className="nav nav-tabs">
-          <li className={activeTabStyle} onClick={this.handleClickActive}>
-            <a data-toggle="tab" href={"#" + activeId}>
-              Active tasks
-            </a>
-          </li>
-          <li className={finishedTabStyle} onClick={this.handleClickFinished}>
-            <a data-toggle="tab" href={"#" + finishedId}>
-              Finished tasks
-            </a>
-          </li>
-        </ul>
-        <div className="tab-content">
-          <ActiveTaskTable
-            id={activeId}
-            tasks={this.props.activeTasks}
-            isActive={activeIsActive}
-          />
-          <FinishedTaskTable
-            id={finishedId}
-            tasks={this.props.finishedTasks}
-            isActive={finishedIsActive}
-          />
-        </div>
+  return (
+    <div className="col-md-8">
+      <ul className="nav nav-tabs">
+        <li className={activeTabStyle} onClick={props.handleClickActive}>
+          <a data-toggle="tab" href={"#" + activeId}>
+            Active tasks
+          </a>
+        </li>
+        <li className={finishedTabStyle} onClick={props.handleClickFinished}>
+          <a data-toggle="tab" href={"#" + finishedId}>
+            Finished tasks
+          </a>
+        </li>
+      </ul>
+      <div className="tab-content">
+        <ActiveTaskTable
+          id={activeId}
+          tasks={props.activeTasks}
+          isActive={activeIsActive}
+        />
+        <FinishedTaskTable
+          id={finishedId}
+          tasks={props.finishedTasks}
+          isActive={finishedIsActive}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-function mapStateToProps(state) {
-  return {
-    activeTab: state.tasks.activeTab
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    changeTab: activeTab => {
-      dispatch(actions.changeTasksActiveTab(activeTab));
-    }
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TaskTables);
+export default TaskTables;
--- src/containers/TaskTablesContainer/TaskTablesContainer.js
+++ src/containers/TaskTablesContainer/TaskTablesContainer.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import TaskTables from "../TaskTables/TaskTables";
 
-const TaskTablesContainer = props => {
-  return (
-    <div>
+import * as actions from "../../store/actions/actions";
+
+class TaskTablesContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClickActive = this.handleClickActive.bind(this);
+    this.handleClickFinished = this.handleClickFinished.bind(this);
+  }
+
+  handleClickActive(e) {
+    this.props.changeTab("active");
+  }
+
+  handleClickFinished(e) {
+    this.props.changeTab("finished");
+  }
+
+  render() {
+    return (
       <TaskTables
-        activeTasks={props.activeTasks}
-        finishedTasks={props.finishedTasks}
+        handleClickActive={this.handleClickActive}
+        handleClickFinished={this.handleClickFinished}
+        activeTasks={this.props.activeTasks}
+        finishedTasks={this.props.finishedTasks}
+        activeTab={this.props.activeTab}
       />
-    </div>
-  );
-};
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
     activeTasks: state.tasks.active,
-    finishedTasks: state.tasks.finished
+    finishedTasks: state.tasks.finished,
+    activeTab: state.tasks.activeTab
   };
 }
 
-export default connect(mapStateToProps)(TaskTablesContainer);
+function mapDispatchToProps(dispatch) {
+  return {
+    changeTab: activeTab => {
+      dispatch(actions.changeTasksActiveTab(activeTab));
+    }
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskTablesContainer);
